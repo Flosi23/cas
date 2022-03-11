78cbfa790bcef208ab8004890e73f3818a31eb92
simon <simon.weckler@mnet-online.de>
2022-03-11 15:55:55 +0100

    feat: setup nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps as NextAppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import Head from "next/head";
+import createEmotionCache from "$app/createEmotionCache";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface AppProps extends NextAppProps {
+	emotionCache?: EmotionCache;
 }
 
-export default MyApp
+export default function App({
+	Component,
+	emotionCache = clientSideEmotionCache,
+	pageProps,
+}: AppProps): JSX.Element {
+	return (
+		<CacheProvider value={emotionCache}>
+			<Head>
+				<title>LSG</title>
+				<meta
+					name="viewport"
+					content="initial-scale=1, width=device-width"
+				/>
+			</Head>
+				<ChakraProvider>
+						<Component {...pageProps} />
+				</ChakraProvider>
+		</CacheProvider>
+	);
+}
